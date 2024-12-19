d1ef3946973a3773ce19d752f2bf880d84dd4e4f
Jhenbert <jvv202726@gmail.com>
2024-12-20 02:22:32 +0800

    chore: test webhook paymongo

--- src/app/api/webhooks/paymongo/route.ts
+++ src/app/api/webhooks/paymongo/route.ts
@@ -1,15 +1,30 @@
 import { NextResponse } from "next/server";
-
+import { createTransaction } from "@/lib/actions/transaction.action";
 // Get webhook secret from environment variable
 // const WEBHOOK_SECRET = process.env.PAYMONGO_WEBHOOK_SECRET_KEY;
 
 export async function POST(request: Request) {
-  try {
-    const payload = await request.json();
-    const { data, type } = JSON.parse(payload);
+  const payload = await request.json();
+  const { data, type } = JSON.parse(payload);
 
-    console.log("data: ", data);
-    console.log("type: ", type);
+  console.log("data: ", data);
+  console.log("type: ", type);
+
+  try {
+    const eventType = data?.attributes?.type;
+
+    if (eventType === "checkout_session.payment.paid") {
+      console.log("Payment created");
+
+      const transaction = {
+        transactionId: data?.id,
+        amount: data?.attributes?.amount ? data?.attributes?.amount / 100 : 0,
+        memberId: data?.attributes?.metadata?.memberId,
+        createdAt: new Date(),
+      };
+
+      await createTransaction(transaction);
+    }
   } catch (error) {
     console.error("Error processing PayMongo webhook:", error);
     return new NextResponse("Internal Server Error", { status: 500 });
